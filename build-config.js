f2e244d38e50d44167874fe6450d0707adbf4916
clown145 <i@kisaki.love>
2026-08-06 05:05:26 +0800

    initial release package base

---
+++ build-config.js
@@ -0,0 +1,8 @@
+// 由 CMake 在链接后生成（platforms/web/gen_build_config.cmake）。请勿手改。
+// 默认值与字段说明见 platforms/web/public/js/config.js。
+window.KRKR2_BUILD_CONFIG = {
+    initialMemory: 67108864,
+    buildVersion: '20260805205334',
+    pwa: true,
+    localZipPicker: false
+};
